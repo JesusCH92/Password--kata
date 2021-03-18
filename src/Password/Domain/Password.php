32c51c6ef470b06838efc99fd4bebc5c769763df
JesusCH92 <jcocaharo@hotmail.com>
2021-03-19 00:51:09 +0100

    adding Password entity

---
+++ src/Password/Domain/Password.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Password\Domain;
+
+final class Password
+{
+    private int $firstNumberValidator;
+    private int $lastNumberValidator;
+    private string $characterValidator;
+    private string $password;
+
+    public function __construct(int $firstNumberValidator, int $lastNumberValidator, string $characterValidator, string $password)
+    {
+        $this->firstNumberValidator = $firstNumberValidator;
+        $this->lastNumberValidator  = $lastNumberValidator;
+        $this->characterValidator   = $characterValidator;
+        $this->password             = $password;
+    }
+
+    public function firstNumberValidator(): int
+    {
+        return $this->firstNumberValidator;
+    }
+
+    public function lastNumberValidator(): int
+    {
+        return $this->lastNumberValidator;
+    }
+
+    public function characterValidator(): string
+    {
+        return $this->characterValidator;
+    }
+
+    public function password(): string
+    {
+        return $this->password;
+    }
+}
