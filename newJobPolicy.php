59037405f182e7e8b7881f21b900af5c4dea3de1
JesusCH92 <jcocaharo@hotmail.com>
2021-03-19 07:10:50 +0100

    creating main script for validate passwords with New Job Policy

---
+++ newJobPolicy.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Password\ApplicationService\PasswordChecker;
+use App\Password\Infrastructure\NewJobPasswordValidatorRepository;
+use App\Password\Infrastructure\PasswordCheckerCommand;
+
+require 'vendor/autoload.php';
+
+
+$passwordCheckerController = new PasswordCheckerCommand(new PasswordChecker(
+    new NewJobPasswordValidatorRepository()
+));
+
+$passwordCheckerController();
